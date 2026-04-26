ecd81fcfc0ff98708ec7bcd960c80f897c7a8edd
flikvion <fesaw357@gmail.com>
2026-04-26 17:23:54 +0300

    initialize

---
+++ OptimizationApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OptimizationApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
