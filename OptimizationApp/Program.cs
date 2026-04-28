2940d37d026dc3acc79685a38bd9d76cc4e34841
Skycoming <skycom.kiko@gmail.com>
2026-04-28 21:49:30 +0300

    add swagger

--- OptimizationApp/Program.cs
+++ OptimizationApp/Program.cs
@@ -9,11 +9,19 @@ var builder = WebApplication.CreateBuilder(args);
 Batteries_V2.Init();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=optimization.db"));
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 using (var scope = app.Services.CreateScope())
 {
